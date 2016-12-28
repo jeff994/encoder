ad41b68a98d64cab74bd1f37158808820431ef40
jeff994 <hujianbo1981@gmai.com>
2016-12-28 17:22:14 +0800

    Fixed the encoder cpp make sure it can re-open the serial port

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -10,31 +10,43 @@
 #include <algorithm>
 #include <iterator>
 
+serial::Serial my_serial; 
+
+void initSerial()
+{
+	my_serial.setPort("/dev/serial/by-id/usb-Arduino__www.arduino.cc__Arduino_Uno_75533353637351616171-if00");
+ 	serial::Timeout::simpleTimeout(1000);
+ 	serial::Timeout timeout	= serial::Timeout::simpleTimeout(1000);
+ 	my_serial.setTimeout(timeout);
+ 	my_serial.setBaudrate (4800);
+}
+
+bool openSerial()
+{
+	if(my_serial.isOpen())
+		return true; 
+	my_serial.open();
+	return my_serial.isOpen();
+} 
+
 int main(int argc, char **argv)
 {
   	ros::init(argc, argv, "encoder");
   	ros::NodeHandle n;
  	ros::Publisher chatter_pub = n.advertise<std_msgs::String>("encoder", 1000);
-	serial::Serial my_serial("/dev/serial/by-id/usb-Arduino__www.arduino.cc__Arduino_Uno_75533353637351616171-if00", 4800, serial::Timeout::simpleTimeout(1000));
 
-   	ros::Rate loop_rate(12);
+ 	initSerial(); 
 
-   	if(my_serial.isOpen())
-    		std::cout << " Yes." << std::endl;
-  	else
-	{
-    		std::cout << " The serial port is not correct." << std::endl;
-		return 0; 
-	}
-
-  	int count = 0;
- 
-
- 	while (true)
+ 	while (ros::ok())
  	{
 		ROS_INFO("Start loop");
+		if(openSerial() == false)
+			usleep(100000);
+			continue; 
 		std_msgs::String msg;
-    		std::string result; 
+
+
+    	std::string result; 
 		
    	 	size_t n_size = my_serial.readline(result); 
 		std::stringstream ss(result);
@@ -45,7 +57,7 @@ int main(int argc, char **argv)
     		 ROS_INFO("%s", result.c_str());
 		if(vstrings.size() != 4)
 		{	
-			std::cout << "message is not correct" << std::endl;
+			ROS_ERROR("Messge %s format is not correct",result.c_str());
 		}
 		else
 		{
@@ -63,10 +75,9 @@ int main(int argc, char **argv)
 			chatter_pub.publish(msg);
 
 		}	
-    		ros::spinOnce();
+    	ros::spinOnce();
 
     		//loop_rate.sleep();
-   		 ++count;
 		ROS_INFO("End loop");
   	}
   	return 0;
